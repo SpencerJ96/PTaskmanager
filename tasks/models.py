53417afbd44ebb71f5b0d4374d4bfd8ef05fa959
SpencerJ96 <spencer-jones1@hotmail.co.uk>
2026-06-29 16:51:02 +0100

    V2 in progress, new layout, models, view and CSS

--- tasks/models.py
+++ tasks/models.py
@@ -1,19 +1,32 @@
 from django.db import models
 
 class Task(models.Model):
+	STATUS_CHOICES = [
+		("notStarted", "Not Started"),
+		("inProgress", "In Progress"),
+		("completed", "Completed"),
+	]
 	PRIORITY_CHOICES = [
 		("low", "Low"),
 		("medium", "Medium"),
-		("high", "High"),
+		("high", "High")
 	]
-
+	CATEGORY_CHOICES = [
+		("work", "Work"),
+		("dog", "Dog Stuff"),
+		("gym", "Gym"),
+		("housework", "Housework"),
+		("shopping", "Shopping"),
+		("miscellaneous", "Miscellaneous")
+	]
 	title = models.CharField(max_length=200)
 	body = models.TextField(blank=True)
 	created = models.DateTimeField(auto_now_add=True)
 	due_date = models.DateField(null=True, blank=True)
-	completed = models.BooleanField(default=False)
 	completed_at = models.DateTimeField(null=True, blank=True)
-	priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES, default="medium")
+	status = models.CharField(max_length=200, choices=STATUS_CHOICES, default="notStarted")
+	priority = models.CharField(max_length=200, choices=PRIORITY_CHOICES, default="low")
+	category = models.CharField(max_length=200, choices=CATEGORY_CHOICES, default="miscellaneous")
 
 	def __str__(self):
 		return self.title
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.utils import timezone
-from datetime import timedelta
 from .models import Task
 
 
@@ -9,50 +8,33 @@ def task_list(request):
 	#Fetches every task from the DB and sorts it so incomplete comes first and higher priority tasks appear before lower
 		#Completed is boolean in the modal (0 is false, 1 is true) we are sorting by ascending(the default) so 0 appears at the top 
 		#"-" in priority means descending so we sort from bottom up in the model (high, med,low)
-	all_tasks = Task.objects.all().order_by("completed", "-priority")	 	
-	#Stores the time 
-	today = timezone.now().date()
-	#Todays date minus 7 days 
-	week_ago = timezone.now() - timedelta(days=7)
+	all_tasks = Task.objects.filter(status__in=["notStarted", "inProgress"]).order_by("status", "-priority")
+	completed_tasks = Task.objects.filter(status="completed")
 
-	#Filter all tasks that are marked as false and give the number.
-	remaining = Task.objects.filter(completed=False).count()
+	completed =  Task.objects.filter(status="completed").count()
+	inProgress = Task.objects.filter(status="inProgress").count()
+	notStarted = Task.objects.filter(status="notStarted").count()
+
+	total = notStarted + inProgress + completed
 	
-	#Filter all tasks that have a "completed" property value of true
-		#Get the Complete at value from the tasks. 
-			#Is the completed at value greater or equal to the value from "week_ago"
-			#E.G TODAYS DATE = 14
-			#	 Completed At = 7 
-			#	 Week_ago = 14 - 7 = 7 
-			# Is completed at greater or equal to a week ago? Yes its equal, include it.
-	completed_this_week = Task.objects.filter(completed=True, completed_at__gte=week_ago).count()
-	#Is the completed false, is the due date property less than Today
-	#Today = 10
-	#Due date is 4 
-	#due date is less than today, inclue it
-	overdue = Task.objects.filter(completed=False, due_date__lt=today).count()
-
-	total_ever = Task.objects.count()
-	total_incomplete = Task.objects.filter(completed=False).count()
 
 
-	#Ternary Operators (Yoda Speak) read it backwards
-		#251 is the stroke of the circle 
-			#completed this week divided by remaining tasks multiplied by 251 to give us the offset px fill circles
-			#True condition on left, false condition on right. Look for the "if"
-	completed_offset = 251 - (completed_this_week / remaining * 251) if remaining > 0 else 251
-	overdue_offset = (overdue / remaining * 251 ) if remaining > 0 else 0
+	
+	completed_offset = 251 - (completed / total * 251) if total > 0 else 251
+	inProgress_offset = 251 - (inProgress / total * 251) if total > 0 else 251
+	notStarted_offset = 251 - (notStarted / total * 251) if total > 0 else 251
 
 
 	#Pass six pieces of data to the template, what to reference them as first and their actual values second
 	return render(request, "tasks/task_list.html", {
 		"all_tasks": all_tasks,
-		"remaining" : remaining,
-		"completed_this_week" : completed_this_week,
-		"overdue" : overdue,
+		"completed" : completed,
+		"completed_tasks" : completed_tasks,
+		"inProgress" : inProgress,
+		"notStarted" : notStarted,
 		"completed_offset" : completed_offset,
-		"overdue_offset" : overdue_offset
-
+		"inProgress_offset" : inProgress_offset,
+		"notStarted_offset" : notStarted_offset
 	})
  
 
@@ -66,18 +48,24 @@ def task_create(request):
 		body = request.POST.get("body", "")
 		priority = request.POST.get("priority", "medium")
 		due_date = request.POST.get("due_date") or None
+		category = request.POST.get("category")
+		status = request.POST.get("status")
 		#Map the form variables to the model variables 
 			#If model item was ticketTitle, it would read ticketTitle=title
 				#The first variable refers to the model, the second the form input variable
-		new_task = Task(title=title, body=body, priority=priority, due_date=due_date)
+		new_task = Task(title=title, body=body, priority=priority, due_date=due_date, category=category, status=status)
 		new_task.save()
 	return redirect("/tasks/")
 
 	
-def task_complete(request, task_id):				#Task_id comes from button in browswer 
+def task_status(request, task_id):				    #Task_id comes from button in browswer 
 	task = Task.objects.get(id=task_id)				#Fetch task from DB pass in task_id. 
-	task.completed = True							#Update completed field
-	task.completed_at = timezone.now()				#Update completed_at
+	if task.status == "notStarted":					
+		task.status = "inProgress"					#If block for changing task status
+	elif task.status == "inProgress":
+		task.status = "completed"										
+		task.completed_at = timezone.now()	        #Update completed_at
+					
 	task.save()										#Save to DB
 	return redirect("/tasks/")						#Redirect to tasks page
 
@@ -86,3 +74,18 @@ def task_delete(request, task_id):
 	deleteTask.delete()
 	return redirect("/tasks/")
 
+def task_all(request):
+	tasks = Task.objects.filter(status__in=["notStarted", "inProgress"])
+	return render (request, "tasks/task_all.html", {"all_tasks" : tasks})
+
+def task_allCompleted(request):
+	tasks = Task.objects.filter(status="completed")
+	return render (request, "tasks/task_allCompleted.html", {"completed_tasks" : tasks})
+
+def task_filter(request):		#Key in URL (HTML) has to match what you're reading in the view
+	taskcategory = request.GET.get("category", "all")  #first GET is Django get method for reading from URL and mapping key-pairs. Second get is python reading the key-pairs
+	if taskcategory == "all":
+		tasks = Task.objects.filter(status__in=["notStarted", "inProgress"]) #If there is no category selected(all in URL) display not started and in prog tasks
+	else:
+		tasks = Task.objects.filter(category=taskcategory, status__in=["notStarted", "inProgress"]) # Filter all tasks where the model category matches the taskcategory variable. and status matches
+	return render(request, "tasks/partials/task_grid.html", {"all_tasks" : tasks}) #Only returning a fragment of the HTML from HTMX, just drop the task grid without reloading anything else 
