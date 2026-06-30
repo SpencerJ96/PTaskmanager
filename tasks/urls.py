99a67751c51079858982750e99dade3f9f3f0531
SpencerJ96 <spencer-jones1@hotmail.co.uk>
2026-06-30 21:46:07 +0100

    V2 Layout and CSS rebuilt

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,10 +1,12 @@
-from django.urls import path #Import paths for routing and views from folder
+from django.urls import path
 from . import views
 
-							#Create urlpatterns array - pass in url path, views function and name for reference in templates
 urlpatterns = [
 	path("", views.task_list, name="task_list"),
 	path("create/", views.task_create, name="task_create"),
-	path("complete/<int:task_id>/", views.task_complete, name="task_complete"),			#Pass in task id to browser for specific object tasks
-	path("delete/<int:task_id>/", views.task_delete, name="task_delete")
+	path("all/", views.task_all, name="task_all"),
+	path("completed/", views.task_allCompleted, name="task_allCompleted"),
+	path("filter/", views.task_filter, name="task_filter"),
+	path("delete/<int:task_id>/", views.task_delete, name="task_delete"),
+	path("status/<int:task_id>/", views.task_status, name="task_status")
 ]
